d15210625ec078e8e6ad621bf60b208b91448dd7
Dalzon Charles-Hébert <charlby5@gmail.com>
2023-01-04 15:46:40 -0500

    Disallow user to go to the login page if already logged in

--- client/pages/se-connecter.js
+++ client/pages/se-connecter.js
@@ -1,8 +1,18 @@
+import { useRouter } from 'next/router';
+
 import Layout from '../components/Layout';
 import FormContainer from '../components/utilities/FormContainer';
 import LoginForm from '../components/form/login';
+import { useMe } from '../context/userContext';
 
 const Login = () => {
+  const { user } = useMe();
+  const router = useRouter();
+
+  if (user) {
+    router.push('/profil');
+  }
+
   return (
     <Layout title='Se connecter'>
       <FormContainer>
