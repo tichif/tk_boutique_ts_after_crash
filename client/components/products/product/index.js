39aa47e4678aea4392d57102a0674e74700fa005
Dalzon Charles-Hébert <charlby5@gmail.com>
2023-01-05 12:25:53 -0500

    Create the Product component

---
+++ client/components/products/product/index.js
@@ -0,0 +1,36 @@
+import Link from 'next/link';
+import { Card } from 'react-bootstrap';
+
+import { getAmountInCurrency } from '../../../utils/number';
+
+const Product = ({ product, currency }) => {
+  return (
+    <Card className='my-3 p-3 rounded'>
+      <Link href={`/article/${product.slug}`}>
+        <Card.Img variant='top' src={product.photoPrincipal.url} />
+      </Link>
+      <Card.Body>
+        <Link href={`/article/${product.slug}`}>
+          <Card.Title as='h4'>
+            <strong>{Product.name}</strong>
+          </Card.Title>
+        </Link>
+        <Card.Text as='h6'>{product.category.name}</Card.Text>
+        <Card.Text as='h6'>
+          HTG {product.price ? product.price : product.variant[0]?.price} -{' '}
+          {getAmountInCurrency(
+            product.price ? product.price : product.variant[0]?.price,
+            currency
+          )}
+        </Card.Text>
+        <Card.Text as='h6'>
+          {(product && product.qty > 0) || product.variant[0].qty
+            ? 'Disponible'
+            : 'Non Disponible'}
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default Product;
