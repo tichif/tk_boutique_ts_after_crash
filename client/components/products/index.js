86cb4c434a14d6e2731b9cd1a9f3b3269d29fab1
Dalzon Charles-Hébert <charlby5@gmail.com>
2023-01-05 12:30:08 -0500

    Create the Products component

---
+++ client/components/products/index.js
@@ -0,0 +1,13 @@
+import { Col } from 'react-bootstrap';
+
+import Product from './product';
+
+const Products = ({ products, currency }) => {
+  return products.map((product) => (
+    <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+      <Product currency={currency} product={product} />
+    </Col>
+  ));
+};
+
+export default Products;
