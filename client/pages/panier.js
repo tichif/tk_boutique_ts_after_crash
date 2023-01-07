b0869d6a52476335e2e8a5c0763e80d9e4cffbc0
Dalzon Charles-Hébert <charlby5@gmail.com>
2023-01-06 19:40:08 -0500

    Show total product and totalPrice in the cart page

--- client/pages/panier.js
+++ client/pages/panier.js
@@ -1,7 +1,8 @@
-import { useEffect } from 'react';
+import { useEffect, useMemo } from 'react';
 import { Row, Col, ListGroup, Button, Card, Alert } from 'react-bootstrap';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
+import { useDispatch, useSelector } from 'react-redux';
 
 import Layout from '../components/Layout';
 import { useMe } from '../context/userContext';
@@ -9,19 +10,34 @@ import { useCart } from '../context/cartContext';
 import Image from '../components/utilities/Image';
 import { getAmountInCurrency } from '../utils/number';
 import { convertDate } from '../utils/date';
+import { wrapper } from '../redux/store';
+import {
+  getPrincipalCurrencyHandler,
+  resetNotifications,
+} from '../redux/actions/currency';
 
 const Panier = () => {
   const router = useRouter();
   const { productId, variantId, qty } = router.query;
+
+  const dispatch = useDispatch();
 
   const {
     state: { cart },
     addToCart,
   } = useCart();
 
+  const { currency, error } = useSelector((state) => state.currencyPrincipal);
+
+  useEffect(() => {
+    if (error) {
+      toast.error(error);
+      dispatch(resetNotifications());
+    }
+  }, [error, dispatch]);
+
   useEffect(() => {
     if (productId || qty) {
-      // dispatch(addToCart(productId, variantId, qty));
       addToCart(productId, variantId, qty);
     }
   }, [productId, variantId, qty]);
@@ -30,6 +46,11 @@ const Panier = () => {
     if (window.confirm('Etes vous sur vouloir supprimer cet article ?')) {
       console.log(key);
     }
+
+    const totalProduct = useMemo(
+      () => cart.reduce((acc, item) => acc + item.qty, 0),
+      [cart]
+    );
   }
   return (
     <Layout title='Panier'>
@@ -90,9 +111,63 @@ const Panier = () => {
             </ListGroup>
           )}
         </Col>
+        <Col md={4}>
+          <Card>
+            <ListGroup variant='flush'>
+              <ListGroup.Item>
+                <h4 style={{ fontSize: '1.1rem', padding: '0.5rem 0' }}>
+                  Sous-total (
+                  {useMemo(
+                    () => cart.reduce((acc, item) => acc + item.qty, 0),
+                    [cart]
+                  )}{' '}
+                  {useMemo(
+                    () => cart.reduce((acc, item) => acc + item.qty, 0),
+                    [cart]
+                  ) > 1
+                    ? 'articles'
+                    : 'article'}
+                  )
+                </h4>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <h4 style={{ fontSize: '1.1rem', padding: '0.5rem 0' }}>
+                  HTG (
+                  {useMemo(
+                    () =>
+                      cart.reduce(
+                        (acc, item) => acc + item.qty * item.price,
+                        0
+                      ),
+                    [cart]
+                  ).toFixed(2)}
+                  -{' '}
+                  {getAmountInCurrency(
+                    useMemo(
+                      () =>
+                        cart.reduce(
+                          (acc, item) => acc + item.qty * item.price,
+                          0
+                        ),
+                      [cart]
+                    ),
+                    currency
+                  )}
+                  )
+                </h4>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
       </Row>
     </Layout>
   );
 };
+
+export const getServerSideProps = wrapper.getServerSideProps(
+  (store) => async (ctx) => {
+    await store.dispatch(getPrincipalCurrencyHandler());
+  }
+);
 
 export default Panier;
