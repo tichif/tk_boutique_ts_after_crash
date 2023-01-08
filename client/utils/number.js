f0efe3ebaa6d836367b81d77dd29fe2c11277d8a
Dalzon Charles-Hébert <charlby5@gmail.com>
2023-01-07 19:29:39 -0500

    Create a cart component

--- client/utils/number.js
+++ client/utils/number.js
@@ -1,3 +1,7 @@
 export function getAmountInCurrency(amount, currency) {
   return `${currency.symbol} ${(amount / currency.amount).toFixed(2)}`;
 }
+
+export function addDecimal(amount) {
+  return (Math.round(amount * 100) / 100).toFixed(2);
+}
