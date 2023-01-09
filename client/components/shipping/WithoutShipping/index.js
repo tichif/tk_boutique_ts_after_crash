6ea01685a3823cacc322553a6186af81425104b7
Dalzon Charles-Hébert <charlby5@gmail.com>
2023-01-09 10:55:19 -0500

    Fix the error in the withoutShipping component

--- client/components/shipping/WithoutShipping/index.js
+++ client/components/shipping/WithoutShipping/index.js
@@ -1,20 +1,24 @@
 import { Button } from 'react-bootstrap';
+import { useRouter } from 'next/router';
 
 import { useOrder } from '../../../context/orderContext';
 
 const WithoutShipping = () => {
   const { addShippingInfos } = useOrder();
+  const router = useRouter();
 
   function clickHandler() {
     addShippingInfos({
       address: 'N/A',
       price: 0,
     });
+    router.push('/methode-paiement');
   }
 
   return (
     <>
-      <Button variant='primary' onClick={clickHandler}>
+      <p>Si vous ne vous pas voulez pas de livraison, cliquez sur continuer</p>
+      <Button variant='primary' onClick={clickHandler} className='my-3'>
         Continuer
       </Button>
     </>
