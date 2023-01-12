ab1d205ee5104beaa299f8af63fbc4d2e943a9c6
Dalzon Charles-Hébert <charlby5@gmail.com>
2023-01-11 21:38:55 -0500

    Disallow shipping page if cart is empty

--- client/components/shipping/index.js
+++ client/components/shipping/index.js
@@ -1,14 +1,27 @@
-import { useState, Suspense, lazy } from 'react';
+import { useState, Suspense, lazy, useEffect } from 'react';
 import { Button } from 'react-bootstrap';
+import { useRouter } from 'next/router';
 
 import CheckoutSteps from '../utilities/CheckoutSteps';
 import Loader from '../utilities/Loader';
+import { useCart } from '../../context/cartContext';
 
 const WithoutShipping = lazy(() => import('../shipping/WithoutShipping'));
 const WithShipping = lazy(() => import('../shipping/WithShipping'));
 
 const Shipping = () => {
   const [shipping, setShipping] = useState(false);
+
+  const { cart } = useCart();
+
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!cart) {
+      router.push('/panier');
+    }
+  }, [cart]);
+
   return (
     <>
       <CheckoutSteps step1 step2 />
